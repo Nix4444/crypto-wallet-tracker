67a3743fae61d1149be579181454079d64e1a460
rohis <rohitsaluja751@gmail.com>
2023-04-18 12:40:05 +0530

    changed APIs
    the received function is perfectly fine, sent function works as well prolly, havent tested it yet

--- Wallet.py
+++ Wallet.py
@@ -9,10 +9,10 @@ channel_id=1087340845700763712
 wallet = 'bc1q96y38ev7uvhmrvapgnl9q95gfr99nnxyldeglg' #btc
 token = 'MTA4NzMzNTMwODIxNzAyNDUzMw.G06GkU.-thUHr12PbYJMNUH2EDEdR2JkMV9Q8Z7M2pp8Q'
 api=f"https://blockchain.info/rawaddr/{wallet}"
-addy_info_api=f"https://api.blockcypher.com/v1/btc/main/addrs/{wallet}"
+#addy_info_api=f"https://api.blockcypher.com/v1/btc/main/addrs/{wallet}" not using blockcypher anymore
 usd_api='https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key={321940b1c8bf5b47d9d121abebceac2f25f0d102c630d559c47e02fc455aed0f}'
-api_token='d0c92a0487f24892a58054c8954d675e'#blockcypher
-test = f"https://blockstream.info/api/address/{wallet}/txs"
+api_token='d0c92a0487f24892a58054c8954d675e'#blockcypher token
+addy_info_api = f"https://blockstream.info/api/address/{wallet}/txs"
 
 
 
@@ -60,10 +60,15 @@ def sent_btc():
 @tasks.loop(seconds=90)
 async def check_trnscs():
     
-    response = requests.get(test)
+    response = requests.get(addy_info_api)
     txs = response.json()
     
     latest_tx = txs[0]["txid"]
+    btc_emoji = discord.utils.get(client.emojis, name='BTC')
+    usdt_emoji = discord.utils.get(client.emojis, name='USDT')
+    usd_emoji = discord.utils.get(client.emojis, name='USD')
+    
+    channel_id=1087340845700763712
     
     
     
@@ -88,62 +93,33 @@ async def check_trnscs():
             
             if direction == "sent":
                 value = address_input_value - address_output_value
+                usd_rate = update_usd()
+                finalp = usd_rate * value * 0.00000001
+                finalp = round(finalp,2)
+                embed = discord.Embed(title="Money Sent Successfully!", color=0xff0000)
+                embed.set_author(name="BTC Sent!", url=f"https://blockchair.com/bitcoin/transaction/{latest_tx}", icon_url='https://cdn-icons-png.flaticon.com/512/5610/5610944.png')
+                embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1888/1888486.png")
+                embed.add_field(name=f"{btc_emoji}BTC", value=value*0.00000001)
+                embed.add_field(name=f"{usd_emoji}Value", value=f"${finalp}", inline=True)
+                embed.add_field(name=f"{usdt_emoji}BTC Price", value=f"${usd_rate}", inline=True)
+                update_oldhash(latest_tx)
+                await client.get_channel(channel_id).send(embed=embed)
             else:
                 value = address_output_value - address_input_value
-           
-        
+                usd_rate = update_usd()
+                finalp = usd_rate * value *0.00000001
+                finalp = round(finalp,2)
+                
+                embed = discord.Embed(title="Money Received Successfully!", color=0x46D117)
+                embed.set_author(name="BTC Received!", url=f"https://blockchair.com/bitcoin/transaction/{latest_tx}", icon_url='https://cdn-icons-png.flaticon.com/512/5610/5610944.png')
+                embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1888/1888486.png")
+                embed.add_field(name=f"{btc_emoji}BTC", value=value*0.00000001)
+                embed.add_field(name=f"{usd_emoji}Value", value=f"${finalp}", inline=True)
+                embed.add_field(name=f"{usdt_emoji}BTC Price", value=f"${usd_rate}", inline=True)
+                update_oldhash(latest_tx)
+                await client.get_channel(channel_id).send(embed=embed)
+            
     
-    
-    
-    
-    
-    '''
-    response = requests.get(api)
-    data = json.loads(response.text)
-    new_transaction = data['txs'][0]
-    new_hash = new_transaction.get('hash')
-    n = new_hash
-    input1 = data['txs'][0]
-    inputs_list = input1.get('inputs')
-    prevout = inputs_list[0].get('prev_out')
-    tx_addy = prevout.get('addr')'''
-    btc_emoji = discord.utils.get(client.emojis, name='BTC')
-    usdt_emoji = discord.utils.get(client.emojis, name='USDT')
-    usd_emoji = discord.utils.get(client.emojis, name='USD')
-    
-    channel_id=1087340845700763712
-    
-    '''if new_hash != oldh:
-        if tx_addy == wallet:
-            btcvalue = sent_btc() * 0.00000001'''
-    usd_rate = update_usd()
-    finalp = usd_rate * value
-    finalp = round(finalp,2)
-            #update_oldhash(new_hash)
-    if direction == "sent":
-            embed = discord.Embed(title="Money Sent Successfully!", color=0xff0000)
-            embed.set_author(name="BTC Sent!", url=f"https://blockchair.com/bitcoin/transaction/{latest_tx}", icon_url='https://cdn-icons-png.flaticon.com/512/5610/5610944.png')
-            embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1888/1888486.png")
-            embed.add_field(name=f"{btc_emoji}BTC", value=value)
-            embed.add_field(name=f"{usd_emoji}Value", value=f"${finalp}", inline=True)
-            embed.add_field(name=f"{usdt_emoji}BTC Price", value=f"${usd_rate}", inline=True)
-            await client.get_channel(channel_id).send(embed=embed)
-
-    else:
-            #btcvalue = get_received_btc(new_hash)
-            usd_rate = update_usd()
-            finalp = usd_rate * value
-            finalp = round(finalp,2)
-            #update_oldhash(new_hash)
-            embed = discord.Embed(title="Money Received Successfully!", color=0x46D117)
-            embed.set_author(name="BTC Received!", url=f"https://blockchair.com/bitcoin/transaction/{latest_tx}", icon_url='https://cdn-icons-png.flaticon.com/512/5610/5610944.png')
-            embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/1888/1888486.png")
-            embed.add_field(name=f"{btc_emoji}BTC", value=value)
-            embed.add_field(name=f"{usd_emoji}Value", value=f"${finalp}", inline=True)
-            embed.add_field(name=f"{usdt_emoji}BTC Price", value=f"${usd_rate}", inline=True)
-            await client.channel_id.send(embed=embed)
-    '''else:
-        return None'''
 
 
 @tasks.loop(seconds = 30)
